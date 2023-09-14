149c102457bfa34856f113ab5cfe972b9fddccbe
Ishan Srivastava <55241531+Ishan998@users.noreply.github.com>
2023-09-14 18:52:23 +0530

    Python API With AUTH 1st Commit

---
+++ api_auth.py
@@ -0,0 +1,183 @@
+from flask import Flask,request
+import json
+from auth_db import authdb as t
+
+app=Flask(__name__)
+
+@app.route('/',methods=['POST','GET'])
+def home():
+    response_home={ "Firstname":"","Surname":"","Username":"","Password":"","Address":"","Pincode":"",
+                   "Phone Number":"","E-mail":""}
+    return response_home
+
+@app.route('/usercreate',methods=['POST'])
+def create():
+    # required is name, surname, username, address, pincode, phone number, e-mail,Password
+    try:
+        res=request.json
+        a=0
+        sn=None
+        fn=None
+        ad=None
+        unh=None
+        pint=0
+        pasw=None
+        Pnu=0
+        mal=None
+        for key,value in list(res.items()):
+            if key=="Firstname":
+                    fn=value
+            if key=="Surname":
+                sn=value
+            if key=="Username":
+                unh=value
+            if key=="Address":
+                ad=value
+            if key=="Pincode":
+                pint=value
+            if key=="Phone Number":
+                Pnu=value 
+            if key=="E-mail":
+                mal=value 
+            if key=="Password":
+                pasw=value    
+        save=t().createuser(fname=fn,surname=sn,addr=ad,pinc=pint,uname=unh,paswd=pasw,Pnumber=Pnu,mail=mal)
+        if save:
+            res.update({'operation':'user created'})
+        else:
+            res.update({'operation':'user not created'})
+        pp=json.dumps(res)
+        return pp
+    except IOError:
+        return Exception
+
+@app.route('/userupdate',methods=['POST'])
+def update():
+    try:
+        res=request.json
+        ids=0
+        sn=None
+        fn=None
+        ad=None
+        unh=None
+        pint=0
+        pasw=None
+        Pnu=0
+        mal=None
+        for key,value in list(res.items()):
+            if key=="id":
+                    # if value!=None:
+                        ids=value 
+            if key=="Firstname":
+                    # if value!=None:
+                        fn=value
+            if key=="Surname":
+                # if value!=None:
+                        sn=value
+            if key=="Username":
+                # if value!=None:
+                        unh=value
+            if key=="Address":
+                # if value!=None:
+                        ad=value
+            if key=="Pincode":
+                # if value!=0:
+                        pint=value
+            if key=="Phone Number":
+                # if value!=0:
+                        Pnu=value 
+            if key=="E-mail":
+                # if value!=None:
+                        mal=value 
+            if key=="Password":
+                # if value!=None:
+                        pasw=value    
+        save=t().updateuser(id=ids,fname=fn,surname=sn,addr=ad,pinc=pint,uname=unh,paswd=pasw,Pnumber=Pnu,mail=mal)
+        if save:
+            res.update({'operation':'user details updated'})
+        else:
+            res.update({'operation':'user details updated'})
+        pp=json.dumps(res)
+        return pp
+    except IOError:
+        return Exception
+
+@app.route('/deleteuser',methods=['DELETE'])
+def deluser():
+    res=request.json
+    for key,value in list(res.items()):
+        if key =="uname":
+            yu=t().deleteuser(id=value)
+            if yu:
+                res.update({'status':'Deleted successfully'})
+            else:
+                res.update({'status':'operation unsuccesfull'})
+    pp=json.dumps(res)
+    return pp
+
+@app.route('/showalluser',methods=['GET'])
+def alluser():
+    try:
+        res=request.json
+        yu=t().readalluser()
+        p=[]
+        res={}
+        data=['id','Firstname','Surname','Address','Pincode','Username','Password','Phone Number','E-mail']
+        for i in yu:
+            res = {data[j]: i[j] for j in range(len(i))}        
+            p.append(res)
+        pp=json.dumps(p)
+        return pp
+    except IOError:
+        return Exception
+    
+@app.route('/showuser',methods=['GET'])
+def showuser():
+    p=[]
+    res=request.json
+    for key,val in list(res.items()):
+        if key=="Username":
+            yu=t().readuser(uname=val)
+            data=['id','Firstname','Surname','Address','Pincode','Username','Password','Phone Number','E-mail']
+            for i in yu:
+                res = {data[j]: i[j] for j in range(len(i))}        
+                p.append(res)
+    pp=json.dumps(res)
+    return pp
+    
+
+@app.route('/deletealluser',methods=['POST'])
+def deleteall():
+    res=request.json
+    for key,val in list(res.items()):
+        if key =="trigger delete all":
+            if val=="True":
+                yu=t().deletealluser()
+                if yu:
+                    res.update({'status':'All records Deleted successfully'})
+                else:
+                    res.update({'status':'operation unsuccesfull'})
+    pp=json.dumps(res)
+    return pp
+
+@app.route('/checkuser',methods=['GET'])
+def authUser():
+    res=request.json
+    un=None
+    pas=None
+    for key,val in list(res.items()):
+        if key =="Username":
+            un=val
+        if key=="Password":
+            pas=val
+        yu=t().auth_user(uname=un,passd=pas)
+        if yu:
+            res.update({'status':'Success'})
+        else:
+            res.update({'status':'Failed'})
+    pp=json.dumps(res)
+    return pp
+
+if __name__=="__main__":
+    app.debug=True
+    app.run(port=1020)
---
+++ auth_db.py
@@ -0,0 +1,179 @@
+import mysql.connector as con
+
+class authdb:
+    def __init__(self):
+        self.host="127.0.0.1"
+        self.user="root"
+        self.password="root"
+        self.database="userAuth"
+        
+    def connection(self):
+        try:
+            self.g=con.connect(host=self.host,user=self.user,password=self.password,database=self.database)
+            self.cur=self.g.cursor()
+            return True
+        except IOError:
+            return Exception
+    def createtable(self):
+        if self.connection():
+            cmd=f"CREATE TABLE UserAuth( ID int AUTO_INCREMENT PRIMARY KEY,FirstName varchar(255),LastName varchar(255) NOT NULL,Address varchar(255),pincode int,Username varchar(255),Password varchar(255),phoneNumber int, Email varchar(255))"
+            try:
+                self.cur.execute(cmd)
+
+                return True
+            except IOError:
+                return Exception
+        else:
+            return ConnectionAbortedError
+
+    def createuser(self,fname,surname,addr,pinc,uname,paswd,Pnumber,mail):
+        if self.connection():
+            val=(fname,surname,addr,pinc,uname,paswd,Pnumber,mail)
+            qr="INSERT INTO UserAuth(Firstname,Lastname,Address,pincode,Username,Password,phoneNumber,Email) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+            try:
+                self.cur.execute(qr,val)
+                self.g.commit()
+                return True
+            except IOError:
+                return Exception
+        else:
+            return ConnectionAbortedError
+        
+    def updateuser(self,id,fname,surname,addr,pinc,uname,paswd,Pnumber,mail):
+        if self.connection()==True:
+            sn=None
+            fn=None
+            ad=None
+            unh=None
+            pint=0
+            pasw=None
+            Pnu=0
+            mal=None
+            try:
+                if surname!=None:
+                    sn=surname
+                else:
+                    return False
+                if fname!=None:
+                    fn=fname
+                if pinc!=0:
+                    pint=pinc
+                if addr!=None:
+                    ad=addr
+                if uname!=None:
+                    unh=uname
+                if paswd!=None:
+                    pasw=paswd
+                if Pnumber!=0:
+                    Pnu=Pnumber
+                if mail!=None:
+                    mal=mail
+                cmd=(f"update UserAuth set FirstName='{fn}',LastName='{sn}',Address='{ad}',pincode={pint},Username='{unh}',Password='{pasw}',phoneNumber={Pnu},Email='{mal}' where ID={id}")
+                self.cur.execute(cmd)
+                self.g.commit()
+                return True
+            except IOError:
+                return Exception
+        else:
+            return ConnectionAbortedError
+        
+    def readuser(self,uname):
+        if self.connection()==True:
+            ad=self.g.cursor()
+            try:
+                t=ad.execute(f"Select * from `UserAuth` where Username='{uname}'")
+                y=ad.fetchall()
+                return y
+            except IOError:
+                return Exception
+        else:
+            return ConnectionAbortedError
+        
+    def readalluser(self):
+        if self.connection()==True:
+            ad=self.g.cursor()
+            try:
+                t=ad.execute("Select * from UserAuth")
+                y=ad.fetchall()
+                return y
+            except IOError:
+                return Exception
+        else:
+            return ConnectionAbortedError
+        
+    def deleteuser(self,id):
+        if self.connection()==True:
+            ad=self.g.cursor()
+            try:
+                cmd=(f"delete from UserAuth where ID={id}")
+                ad.execute(cmd)
+                self.g.commit()
+                y=ad.fetchall()
+                return True
+            except IOError:
+                return Exception
+        else:
+            return ConnectionAbortedError
+        
+    def deletealluser(self):
+        if self.connection()==True:
+            ad=self.g.cursor()
+            try:
+                ad.execute("DELETE from UserAuth")
+                self.g.commit()
+                print("DELETED")
+                return True
+            except IOError:
+                return Exception
+        else:
+            return ConnectionAbortedError
+        
+    def deletable(self):
+        if self.connection()==True:
+            ad=self.g.cursor()
+            try:
+                ad.execute("DROP TABLE UserAuth")
+                self.g.commit()
+                print("DELETED")
+                return True
+            except IOError:
+                return Exception
+        else:
+            return ConnectionAbortedError
+
+    def auth_user(self,uname,passd):
+        if self.connection()==True:
+            ad=self.g.cursor()
+            if uname!=None and passd!=None:
+                try:
+                    ad.execute(f"SELECT Password from UserAuth where Username='{uname}'")
+                    check=ad.fetchone()
+                    check2=check
+                    print(check)
+                    for i in check2:
+                        # print(type(i))
+                        if passd==i:
+                            return True
+                        else:
+                            return False
+                except IOError:
+                    return Exception
+            else:
+                return False
+        else:
+            return ConnectionAbortedError
+
+if __name__=="__main__":
+    au=authdb()
+    # p=au.deletealluser()
+    # p=au.createtable()
+    # p=au.deletable()
+    # p=au.readalluser()
+    # p=au.readuser(uname="varunHample03",ids='1')
+    # p=au.auth_user(uname="varunHample03",passd="test@123")
+    p=au.auth_user(uname="Aman.Trivedi)3",passd="test@123")
+  
+    # p=au.createuser(fname='Varun',surname='hample',addr="hill top near sarana road solan himanchal pradesh",
+    #                     pinc=440102,uname="varunHample03",paswd="test@123",Pnumber=1234567890,mail="varun.hample03@gmail.com")
+    print(p)
+    # print(au.createtable())
